98faebf9c4fef2bbadfb5287ac774ab6d65dd315
Тузовский Анатолий Федорович <63976413+tuzovsky-lab@users.noreply.github.com>
2023-04-20 11:55:05 +0300

    Первая версия проекта 22ISappProject

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace _22ISappProject;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Как вас зовут?");
+     string name = Console.ReadLine();
+     DateTime currentDate = DateTime.Now;
+     Console.WriteLine($"\n Привет {name}, сейчас {currentDate:d} at {currentDate:t}!");
+     Console.Write($"\n Нажмите любую клавишу для завершения...");
+     Console.ReadKey(true);
+
+    }
+}
